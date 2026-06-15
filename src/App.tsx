8603d94b5e008aa9479097a45890626c86308d4b
yoshizawa0108 <wakako.yoshizawa.work@gmail.com>
2026-06-15 16:27:11 +0900

    fix:component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { Layout } from "./components/layout/Layout";
-import { DashboardPage } from "./components/dashboard/Dashboardpage";
-import { CompaniesPage } from "./components/companies/Companiespage";
-import { ApplicationsPage } from "./components/applications/Applicationspage";
-import { InterviewsPage } from "./components/interviews/Interviewspage";
+import { DashboardPage } from "./components/dashboard/DashboardPage";
+import { CompaniesPage } from "./components/companies/CompaniesPage";
+import { ApplicationsPage } from "./components/applications/ApplicationsPage";
+import { InterviewsPage } from "./components/interviews/InterviewsPage";
 
 type Page = "dashboard" | "companies" | "applications" | "interviews";
 export default function App() {
